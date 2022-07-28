59766bc3180ada634b07f235eb23fafd893b4024
philona123 <philonasebastian44@gmail.com>
2022-07-28 18:01:42 +0530

    Fixed Username conflicts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 
@@ -10,4 +11,22 @@ export class AuthService {
         @InjectRepository(User)
         private usersRepository: Repository<User>,
     ) {}
+
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> { // signup
+        const { username, password } = authCredentialsDto;
+        const user = this.usersRepository.create({
+            username,
+            password,
+        });
+        try {
+            await this.usersRepository.save(user);
+        }catch(error){
+            if(error.code === '23505'){
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+        
+    }
 }
